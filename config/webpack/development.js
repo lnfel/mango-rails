65a271013126d242c76a08a15de94214671bea5f
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2020-06-25 18:41:22 +0800

    Enable Hot Module Reloading

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+// Live reload
+// generate source maps without the original source file inside them,
+// allowing us to link it directly in the browser
+if (!module.hot) {
+	environment.loaders.get('sass').use.find(item => item.loader === 'sass-loader').options.sourceMapContents = false;
+}
+
 module.exports = environment.toWebpackConfig()
